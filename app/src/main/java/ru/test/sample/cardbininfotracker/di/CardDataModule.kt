5954e2ceabd13091c5c1a933cfb701ffbda9e6fa
Vadim Khimilonov <regester_account@mail.ru>
2024-04-10 04:19:36 +0300

    di: add hilt

---
+++ app/src/main/java/ru/test/sample/cardbininfotracker/di/CardDataModule.kt
@@ -0,0 +1,4 @@
+package ru.test.sample.cardbininfotracker.di
+
+class CardDataModule {
+}
---
+++ app/src/main/java/ru/test/sample/cardbininfotracker/model/BankPresentationModel.kt
@@ -0,0 +1,8 @@
+package ru.test.sample.cardbininfotracker.model
+
+data class BankPresentationModel(
+    val name: String = "",
+    val url: String = "",
+    val phone: String = "",
+    val city: String = "",
+)
