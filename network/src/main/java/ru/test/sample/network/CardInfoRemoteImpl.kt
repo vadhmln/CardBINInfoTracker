35bc54b78bcdd8568d25f4760f9d2abd73fe9dcf
Vadim Khimilonov <regester_account@mail.ru>
2024-04-12 07:36:47 +0300

    refactor: network layer

--- network/src/main/java/ru/test/sample/network/CardInfoRemoteImpl.kt
+++ network/src/main/java/ru/test/sample/network/CardInfoRemoteImpl.kt
@@ -1,20 +1,19 @@
 package ru.test.sample.network
 
-import android.util.Log
 import com.skydoves.sandwich.ApiResponse
 import com.skydoves.sandwich.mapSuccess
 import ru.test.sample.data.datasource.CardInfoRemote
 import ru.test.sample.data.model.CardInfoDataModel
 import ru.test.sample.network.mapper.CardInfoRemoteToDataMapper
+import ru.test.sample.network.model.CardInfoResponse
 
 class CardInfoRemoteImpl(
     private val cardInfoService: CardInfoService,
     private val cardInfoRemoteToDataMapper: CardInfoRemoteToDataMapper,
-): CardInfoRemote {
+) : CardInfoRemote {
 
-    override suspend fun getCardInfo(bin: String): ApiResponse<CardInfoDataModel> {
-       val cardInfo = cardInfoService.getCardInfo(bin).mapSuccess(cardInfoRemoteToDataMapper::toData)
-        Log.d("CardInfoRemoteImpl", "$cardInfo")
-        return cardInfo
-    }
+    override suspend fun getCardInfo(bin: String): ApiResponse<CardInfoDataModel> =
+        cardInfoService.getCardInfo(bin)
+            .mapSuccess<CardInfoResponse, CardInfoDataModel>(cardInfoRemoteToDataMapper::toData)
+
 }
